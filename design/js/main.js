248261c39e74c1225658a02468cc0ab8f281cbe5
Sergii Getmanchuk <getser79@gmail.com>
2016-04-25 23:30:56 +0300

    design html files ready to get data from server

--- design/js/main.js
+++ design/js/main.js
@@ -1,20 +1,51 @@
-var model = {users:[
-          { id: "1", last_name: "Getmanchuk", first_name: "Sergii", second_name: "Oleksandrovich", date_of_birth: "1979-10-09", e_mail: "*****@gmail.com"},
-          { id: "2", last_name: "Umanets", first_name: "Andriy", second_name: "Ivanovich", date_of_birth: "1979-01-25", e_mail: "*****@mail.ru" },
-          { id: "3", last_name: "Donchenko", first_name: "Vitalii", second_name: "Oleksandrovich", date_of_birth: "1978-12-09", e_mail: "*****@hotmail.com"},
-          { id: "5", last_name: "Getman", first_name: "Serg", second_name: "Drovich", date_of_birth: "1989-10-09", e_mail: "*****@gmail.ua" },
-          { id: "8", last_name: "Manchuk", first_name: "Oleksandr", second_name: "Sergiyovich", date_of_birth: "1979-10-22", e_mail: "*-*-*-*-*@gmail.com"}
-      ]};
+// var api_data;
+
+// $http.get("http://127.0.0.1:8000/api/v1/user/?format=json").success(function(response){api_data = response;
+//   });
+
+// var model = {};
+
+// model.users = api_data.objects;
+
+// var model = {users:[
+//           { id: "1", last_name: "Getmanchuk", first_name: "Sergii", second_name: "Oleksandrovich", date_of_birth: "1979-10-09", e_mail: "*****@gmail.com"},
+//           { id: "2", last_name: "Umanets", first_name: "Andriy", second_name: "Ivanovich", date_of_birth: "1979-01-25", e_mail: "*****@mail.ru" },
+//           { id: "3", last_name: "Donchenko", first_name: "Vitalii", second_name: "Oleksandrovich", date_of_birth: "1978-12-09", e_mail: "*****@hotmail.com"},
+//           { id: "5", last_name: "Getman", first_name: "Serg", second_name: "Drovich", date_of_birth: "1989-10-09", e_mail: "*****@gmail.ua" },
+//           { id: "8", last_name: "Manchuk", first_name: "Oleksandr", second_name: "Sergiyovich", date_of_birth: "1979-10-22", e_mail: "*-*-*-*-*@gmail.com"}
+//       ]};
 
 // Модуль
 
 var UserDBApp = angular.module("UserDBApp", []);
 
+console.log("presponse0")
 // Контроллер
 
-UserDBApp.controller("UserDBAppCtrl", function ($scope) {
+UserDBApp.controller("UserDBAppCtrl", function ($scope, $http) {
+  console.log("presponse4")
+  var promise =  $http.get("http://127.0.0.1:8000/api/v1/user/?format=json");
+  promise.then(fulfilled, rejected)
+  console.log("presponse1")
+  function fulfilled() {
+    console.log("Status: " + response.status);
+    console.log("Type: " + response.headers("content-type"));
+    console.log("Length: " + response.headers("content-length"));
 
-  $scope.data = model;
+    var model = {users:[]};
+    model.users = presponse.data.objects;
+    console.log("presponse2")
+    $scope.data = model;
+
+    }
+
+  function rejected(error) {
+    console.log("presponse3")
+    console.error(error.status);
+    console.error(error.statusText);
+
+    }
+
 
   // Обработчик нажатия по кнопке
   $scope.addNewUser = function () {
