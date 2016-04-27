840f39960596b00d3c85ac04a2bc3eb2df247a3e
Sergii Getmanchuk <getser79@gmail.com>
2016-04-27 23:52:11 +0300

    index.html is now creating users on server

--- users/api.py
+++ users/api.py
@@ -1,7 +1,17 @@
+from tastypie.constants import ALL
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
-from .models import User
+from .models import User as Us
 
 class UserResource(ModelResource):
     class Meta:
-        queryset = User.objects.all()
+        queryset = Us.objects.all()
         resourse_name = 'user'
+        allowed_methods = ['get', 'post', 'put', 'delete']
+        authentication = Authentication()
+        authorization = Authorization()
+        filtering = {
+            'id': ALL,
+            'last_name': ALL,
+        }
