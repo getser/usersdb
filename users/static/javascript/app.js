d7c4d94d6c1b767de22acc846929831d6ab3ba1a
Sergii Getmanchuk <getser79@gmail.com>
2016-04-30 00:07:08 +0300

    the next try to implement .xlsx files uploading

--- users/static/javascript/app.js
+++ users/static/javascript/app.js
@@ -1,11 +1,36 @@
   // Модуль
-var UserDBApp = angular.module("UserDBApp", [])
+var UserDBApp = angular.module("UserDBApp", ["ng-file-model"])
 
 .constant("baseUrl", "http://127.0.0.1:8000/api/v1/user/")
 .constant("suffixUrl", "?format=json")
 
+// Сервис
+UserDBApp.factory("XLSXReaderService", ['$q', '$rootScope',
+    function($q, $rootScope) {
+        var service = function(data) {
+            angular.extend(this, data);
+        }
+
+        service.readFile = function(file, readCells, toJSON) {
+            var deferred = $q.defer();
+
+            XLSXReader(file, readCells, toJSON, function(data) {
+                $rootScope.$apply(function() {
+                    deferred.resolve(data);
+                });
+            });
+
+            return deferred.promise;
+        }
+
+
+        return service;
+    }
+]);
+
+
 // Контроллер
-UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUrl) {
+UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUrl, XLSXReaderService) {
 
   $scope.initFirst=function(){
 
@@ -27,6 +52,12 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
       console.error(error.status);
       console.error(error.statusText);
       }
+
+    $scope.choosingFields = null
+    $scope.editUserIndex = null
+
+    $scope.submittedFile = {};
+    $scope.obj = {};
     }
 
   // Обработчик нажатия по кнопке addNewUser
@@ -42,11 +73,9 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
         }
 
       var request = $http.post(baseUrl + suffixUrl, user)
-      request.success(function ( ){
+      request.success(function (){
 
         // console.log(JSON.stringify(user))
-
-        // $scope.data.users.push(user);
 
         $scope.LAST_NAME = ""
         $scope.FIRST_NAME = ""
@@ -62,25 +91,11 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
   // Обработчик editUserSwitcher
   $scope.editUserSwitcher = function (ID, index) {
     $scope.editUserIndex = index
-
-    // $scope.LAST_NAME_EDIT = $scope.data.users[index].last_name
-    // $scope.FIRST_NAME_EDIT = $scope.data.users[index].first_name
-    // $scope.SECOND_NAME_EDIT = $scope.data.users[index].middle_name
-    // $scope.DATE_OF_BIRTH_EDIT = $scope.data.users[index].birthday
-    // $scope.E_MAIL_EDIT = $scope.data.users[index].e_mail
   }
 
   // Обработчик нажатия по кнопке editUser
   $scope.editUser = function (ID, index) {
-  //
-    // $scope.data.users[index].last_name = $scope.LAST_NAME_EDIT
-    // $scope.data.users[index].first_name = $scope.FIRST_NAME_EDIT
-    // $scope.data.users[index].middle_name = $scope.SECOND_NAME_EDIT
-    // $scope.data.users[index].birthday = $scope.DATE_OF_BIRTH_EDIT
-    // $scope.data.users[index].e_mail = $scope.E_MAIL_EDIT
 
-
-    // if ($scope.LAST_NAME_EDIT && $scope.FIRST_NAME_EDIT && $scope.DATE_OF_BIRTH_EDIT && $scope.E_MAIL_EDIT){
     if ($scope.data.users[index].last_name && $scope.data.users[index].first_name && $scope.data.users[index].birthday && $scope.data.users[index].e_mail){
         var user = {
           id: ID,
@@ -89,11 +104,10 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
           middle_name: $scope.data.users[index].middle_name,
           birthday: $scope.data.users[index].birthday,
           e_mail: $scope.data.users[index].e_mail
-          // e_mail: "no@mail.not"
         }
 
-      console.log(JSON.stringify(user))
-      console.log($scope.data.users[index])
+      // console.log(JSON.stringify(user))
+      // console.log($scope.data.users[index])
 
       var request = $http.put(baseUrl + ID + '/' + suffixUrl, user)
       request.success(function (){
@@ -108,19 +122,85 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
         // $scope.initFirst() //commented as not needed...
 
       }); 
-  }
-}
+  }}
+  
 
   // Обработчик нажатия по кнопке deleteUser
   $scope.deleteUser = function (ID, index) {
     // console.log(ID)
-
       var request = $http.delete(baseUrl + ID + '/' + suffixUrl)
       request.success(function (){
 
         $scope.data.users.splice(index, 1);
 
       }); 
-    }  
+    }
+
+
+
+  // $scope.UpLoad = function(obj){
+    
+  //   $scope.excelFile = obj.File;
+
+  //   // console.log(JSON.stringify($scope.excelFile))
+  //   console.log(XLSX.version)
+
+  //   var workbook = XLSX.read($scope.excelFile, {type: "binary"})
+
+  //   console.log(JSON.stringify(workbook))
+
+
+
+
+
+  $scope.UpLoad = function(obj){
+    $scope.json_string = "";
+    $scope.sheets = [];
+    $scope.excelFile = obj[0];
+    $scope.selectedSheetName = 1
+
+    // console.log(JSON.stringify($scope.excelFile))
+    // console.log(XLSX.version)
+    console.log($scope.excelFile.name)
+
+    XLSXReaderService.readFile($scope.excelFile, false, true).then(function(xlsxData) {
+    // console.log("Hello there!")
+
+      $scope.sheets = xlsxData.sheets;
+        });
+
+      // $scope.json_string = JSON.stringify($scope.sheets[$scope.selectedSheetName], null, 2);
+      // $scope.json_string = JSON.stringify($scope.sheets, null, 2);
+
+
+
+    // console.log($scope.json_string)
+
+    for (sheet in $scope.sheets){
+    console.log(JSON.stringify(sheet, null, 2))
+
+    }
+
+
+  //   var fr = new FileReader()
+  //   var arr = fr.readAsArrayBuffer($scope.excelFile)
+
+  //   // console.log(JSON.stringify(arr))
+
+    // var workbook = XLSX.readFile($scope.excelFile.name, {type: "base64"})
+
+    // console.log(JSON.stringify(workbook))
+
+    // $scope.excelFile = obj[0];
+    // console.log(JSON.stringify($scope.excelFile))
+
+    // var workbook = XLSX.readFile($scope.excelFile.name);
+
+    // var workbook = XLSX.utils.sheet_to_json($scope.excelFile);
+    // console.log(JSON.stringify(workbook))
+
+
+    $scope.choosingFields = 1
+  }
 
 });
