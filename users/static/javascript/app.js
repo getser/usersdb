1bfa77acf38a3e057962cbcf9d4c88702b1cfb02
Sergii Getmanchuk <getser79@gmail.com>
2016-04-29 11:55:10 +0300

    user edidting from list works (without validation notifications)

--- users/static/javascript/app.js
+++ users/static/javascript/app.js
@@ -31,6 +31,7 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
 
   // Обработчик нажатия по кнопке addNewUser
   $scope.addNewUser = function () {
+    $scope.editUserIndex = null
     if ($scope.LAST_NAME && $scope.FIRST_NAME && $scope.DATE_OF_BIRTH && $scope.E_MAIL){
         var user = {
           last_name: $scope.LAST_NAME,
@@ -62,11 +63,11 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
   $scope.editUserSwitcher = function (ID, index) {
     $scope.editUserIndex = index
 
-    $scope.LAST_NAME_EDIT = $scope.data.users[index].last_name
-    $scope.FIRST_NAME_EDIT = $scope.data.users[index].first_name
-    $scope.SECOND_NAME_EDIT = $scope.data.users[index].middle_name
-    $scope.DATE_OF_BIRTH_EDIT = $scope.data.users[index].birthday
-    $scope.E_MAIL_EDIT = $scope.data.users[index].e_mail
+    // $scope.LAST_NAME_EDIT = $scope.data.users[index].last_name
+    // $scope.FIRST_NAME_EDIT = $scope.data.users[index].first_name
+    // $scope.SECOND_NAME_EDIT = $scope.data.users[index].middle_name
+    // $scope.DATE_OF_BIRTH_EDIT = $scope.data.users[index].birthday
+    // $scope.E_MAIL_EDIT = $scope.data.users[index].e_mail
   }
 
   // Обработчик нажатия по кнопке editUser
@@ -79,21 +80,20 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
     // $scope.data.users[index].e_mail = $scope.E_MAIL_EDIT
 
 
-    if ($scope.LAST_NAME_EDIT && $scope.FIRST_NAME_EDIT && $scope.DATE_OF_BIRTH_EDIT && $scope.E_MAIL_EDIT){
+    // if ($scope.LAST_NAME_EDIT && $scope.FIRST_NAME_EDIT && $scope.DATE_OF_BIRTH_EDIT && $scope.E_MAIL_EDIT){
+    if ($scope.data.users[index].last_name && $scope.data.users[index].first_name && $scope.data.users[index].birthday && $scope.data.users[index].e_mail){
         var user = {
           id: ID,
-          last_name: $scope.LAST_NAME_EDIT,
-          first_name: $scope.FIRST_NAME_EDIT,
-          middle_name: $scope.SECOND_NAME_EDIT,
-          birthday: $scope.DATE_OF_BIRTH_EDIT,
-          e_mail: $scope.E_MAIL_EDIT
+          last_name: $scope.data.users[index].last_name,
+          first_name: $scope.data.users[index].first_name,
+          middle_name: $scope.data.users[index].middle_name,
+          birthday: $scope.data.users[index].birthday,
+          e_mail: $scope.data.users[index].e_mail
           // e_mail: "no@mail.not"
         }
 
       console.log(JSON.stringify(user))
       console.log($scope.data.users[index])
-      // console.log(document.getElementById('lname_edit' + index).innerHTML)
-      console.log('lname_edit' + index)
 
       var request = $http.put(baseUrl + ID + '/' + suffixUrl, user)
       request.success(function (){
@@ -105,10 +105,11 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
         $scope.DATE_OF_BIRTH_EDIT = ""
         $scope.E_MAIL_EDIT = ""
 
-        // $scope.initFirst() //commeneted till problems exist...
+        // $scope.initFirst() //commented as not needed...
 
       }); 
-  }}
+  }
+}
 
   // Обработчик нажатия по кнопке deleteUser
   $scope.deleteUser = function (ID, index) {
