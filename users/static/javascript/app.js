0b8a198c732e3a9a828b5a55300620c7556dac38
Sergii Getmanchuk <getser79@gmail.com>
2016-04-29 10:38:27 +0300

    users list is reloading on adding new uaser

--- users/static/javascript/app.js
+++ users/static/javascript/app.js
@@ -7,23 +7,26 @@ var UserDBApp = angular.module("UserDBApp", [])
 // Контроллер
 UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUrl) {
 
-  var promise =  $http.get(baseUrl + suffixUrl);
-  promise.then(fulfilled, rejected)
+  $scope.initFirst=function(){
 
-  function fulfilled(response) {
-    console.log("Status: " + response.status);
-    console.log("Type: " + response.headers("content-type"));
-    console.log("Length: " + response.headers("content-length"));
+    var promise =  $http.get(baseUrl + suffixUrl);
+    promise.then(fulfilled, rejected)
 
-    var model = {users:[]};
-    model.users = response.data.objects;
-    $scope.data = model;
-    }
+    function fulfilled(response) {
+      console.log("Status: " + response.status);
+      console.log("Type: " + response.headers("content-type"));
+      console.log("Length: " + response.headers("content-length"));
 
-  function rejected(error) {
-    console.log("presponse3")
-    console.error(error.status);
-    console.error(error.statusText);
+      var model = {users:[]};
+      model.users = response.data.objects;
+      $scope.data = model;
+      }
+
+    function rejected(error) {
+      console.log("presponse3")
+      console.error(error.status);
+      console.error(error.statusText);
+      }
     }
 
   // Обработчик нажатия по кнопке addNewUser
@@ -42,13 +45,15 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
 
         // console.log(JSON.stringify(user))
 
-        $scope.data.users.push(user);
+        // $scope.data.users.push(user);
 
         $scope.LAST_NAME = ""
         $scope.FIRST_NAME = ""
         $scope.SECOND_NAME = ""
         $scope.DATE_OF_BIRTH = ""
         $scope.E_MAIL = ""
+
+        $scope.initFirst()
 
       });   
   }}  
@@ -67,11 +72,11 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
   // Обработчик нажатия по кнопке editUser
   $scope.editUser = function (ID, index) {
   //
-    $scope.data.users[index].last_name = $scope.LAST_NAME_EDIT
-    $scope.data.users[index].first_name = $scope.FIRST_NAME_EDIT
-    $scope.data.users[index].middle_name = $scope.SECOND_NAME_EDIT
-    $scope.data.users[index].birthday = $scope.DATE_OF_BIRTH_EDIT
-    $scope.data.users[index].e_mail = $scope.E_MAIL_EDIT
+    // $scope.data.users[index].last_name = $scope.LAST_NAME_EDIT
+    // $scope.data.users[index].first_name = $scope.FIRST_NAME_EDIT
+    // $scope.data.users[index].middle_name = $scope.SECOND_NAME_EDIT
+    // $scope.data.users[index].birthday = $scope.DATE_OF_BIRTH_EDIT
+    // $scope.data.users[index].e_mail = $scope.E_MAIL_EDIT
 
 
     if ($scope.LAST_NAME_EDIT && $scope.FIRST_NAME_EDIT && $scope.DATE_OF_BIRTH_EDIT && $scope.E_MAIL_EDIT){
@@ -99,6 +104,8 @@ UserDBApp.controller("UserDBAppCtrl", function ($scope, $http, baseUrl, suffixUr
         $scope.SECOND_NAME_EDIT = ""
         $scope.DATE_OF_BIRTH_EDIT = ""
         $scope.E_MAIL_EDIT = ""
+
+        // $scope.initFirst() //commeneted till problems exist...
 
       }); 
   }}
